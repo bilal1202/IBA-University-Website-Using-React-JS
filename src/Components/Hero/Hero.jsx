ce9de258bf96d6db9f7c0612736e3b6a8d03d638
Muhammad Bilal <87284041+bilal1202@users.noreply.github.com>
2024-07-12 05:30:50 +0500

    Created Navigation Hero and Pograms offering section

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Hero.css'
+
+
+const Hero = () => {
+  return (
+    <div className='hero container'>
+        <div className="hero-text">
+            <h1>Fostering Excellence in Education and Future Leadership</h1>
+            <p>Our mission is to nurture and develop outstanding educational experiences, empowering the leaders of tomorrow with the skills and knowledge they need to succeed.</p>
+            <button className='btn'>Explore more</button>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
