c05c7d7a9e2df66c0b42cd1f952610d6f1c4fd43
Muhammad Bilal <87284041+bilal1202@users.noreply.github.com>
2024-08-03 04:42:38 +0500

    completed footer & react scroll part

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import About from './Components/About/About'
 import Campus from './Components/Campus/Campus'
 import Testimonials from './Components/Testimonials/Testimonials'
 import Contact from './Components/Contact/Contact'
+import Footer from './Components/Footer/Footer'
 
 
 
@@ -28,6 +29,7 @@ const App = () => {
       <Testimonials/>
       <Title  subTitle='Contact Us'  title='Get In Touch' />
       <Contact/>
+      <Footer/>
       </div>
     </div>
   )
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
+import { Link } from 'react-scroll'
 
 const Navbar = () => {
   return (
     <nav className='container'>
         <img src={logo} alt="" className='logo' />
         <ul>
-            <li>Home</li>
-            <li>Programs</li>
-            <li>About</li>
-            <li>Campus</li>
-            <li>Testimonials</li>
-            <li> <button className='btn'>Contact us </button></li>
+            <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
+            <li><Link to='program' smooth={true} offset={-260} duration={500}>Programs</Link></li>
+            <li><Link to='about' smooth={true} offset={-150} duration={500}>About</Link></li>
+            <li><Link to='campus' smooth={true} offset={-260} duration={500}>Campus</Link></li>
+            <li><Link to='testimonials' smooth={true} offset={-260} duration={500}>Testimonials</Link></li>
+             <li> <Link to='testimonials' smooth={true} offset={60} duration={500}> <button className='btn'>Contact us </button> </Link></li>
 
 
         </ul>
